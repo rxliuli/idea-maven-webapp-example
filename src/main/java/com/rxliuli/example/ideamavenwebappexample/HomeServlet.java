2b6578b53bfa58947b23c83d498345d03dd8b3c0
rxliuli <rxliuli@gmail.com>
2018-12-04 20:24:23 +0800

    Initial commit

---
+++ src/main/java/com/rxliuli/example/ideamavenwebappexample/HomeServlet.java
@@ -0,0 +1,28 @@
+package com.rxliuli.example.ideamavenwebappexample;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author rxliuli
+ */
+@WebServlet(name = "HomeServlet", urlPatterns = "/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        final PrintWriter out = resp.getWriter();
+        out.println("这里是首页哦");
+    }
+}
